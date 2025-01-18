5b6b95c3409dbab9e1b5a7de271eb4652ab8ad5e
shashank <139876587+msshashank1997@users.noreply.github.com>
2025-01-18 20:04:43 +0530

    updted file

---
+++ auth.py
@@ -0,0 +1,7 @@
+print("just an suth code")
+#if condtion to check age is above 18 or not
+age = 18
+if age > 18:
+    print("you are eligible to vote")
+else:
+    print("you are not eligible to vote")
